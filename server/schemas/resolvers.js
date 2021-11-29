8bfb0ef8a27ef8c49a0fbe871494e85953e12914
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-28 23:11:09 -0500

    creating mutations for user creation and login, removing and adding a post, and adding a comment and removing a comment

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,6 +19,62 @@ const resolvers = {
     },
   },
 
-}
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addPost: async (parent, { postBody, postAuthor }) => {
+      const post = await Post.create({ postBody, postAuthor });
+
+      await User.findOneAndUpdate(
+        { username: postAuthor },
+        { $addToSet: { posts: post._id } }
+      );
+
+      return post;
+    },
+    addComment: async (parent, { postId, commentText, commentAuthor }) => {
+      return Post.findOneAndUpdate(
+        { _id: postId },
+        {
+          $addToSet: { comments: { commentText, commentAuthor } },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+    removePost: async (parent, { postId }) => {
+      return Post.findOneAndDelete({ _id: postId });
+    },
+    removeComment: async (parent, { postId, commentId }) => {
+      return postId.findOneAndUpdate(
+        { _id: postId },
+        { $pull: { comments: { _id: commentId } } },
+        { new: true }
+      );
+    },
+  },
+};
 
 module.exports = resolvers;
