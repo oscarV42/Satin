d6dbca0c6d96d0e45586cfcff769faae26e7e4f6
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-30 18:48:16 -0500

    installing dependencies and creating a function that checks if a token exists and if it is not expired

---
+++ client/src/utils/auth.js
@@ -0,0 +1,14 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    // If there is a token and it's not expired, return `true`
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
+
+}
