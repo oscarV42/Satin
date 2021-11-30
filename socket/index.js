ddb447fefd3f52562491f605dc03455d58b47e97
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-11-30 15:03:10 -0500

    creating addUser function that takes in userId and socketId

--- socket/index.js
+++ socket/index.js
@@ -2,4 +2,12 @@ const io = require("socket.io")(8900, {
     cors: {
       origin: "http://localhost:3000",
     },
-  });
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+  users.push({ userId, socketId });
+};
+  
