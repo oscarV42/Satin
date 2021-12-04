bbed51c114b88d4f43d665bc35030576eccb6e2a
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-12-03 20:42:07 -0500

    using ApolloProvider so the frontend can make queries or mutations

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,65 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import Messenger from "./pages/messenger/Messenger";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
-import Messenger from "./pages/messenger/Messenger";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+// Construct main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Holder ${token}` : '',
+    }
+  }
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   const { user } = useContext(AuthContext);
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Register />}
-        </Route>
-        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
-        <Route path="/register">
-          {user ? <Redirect to="/" /> : <Register />}
-        </Route>
-        <Route path="/messenger">
-          {!user ? <Redirect to="/" /> : <Messenger />}
-        </Route>
-        <Route path="/profile/:username">
-          <Profile />
-        </Route>
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {user ? <Home /> : <Register />}
+          </Route>
+          <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+          <Route path="/register">
+            {user ? <Redirect to="/" /> : <Register />}
+          </Route>
+          <Route path="/messenger">
+            {!user ? <Redirect to="/" /> : <Messenger />}
+          </Route>
+          <Route path="/profile/:username">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
