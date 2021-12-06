691e385a2de78cc12dee0b3ce9a2d0786e6fdce6
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-12-06 15:54:47 -0500

    exporting queries and resolvers

---
+++ server/schemas/resolvers/index.js
@@ -0,0 +1,19 @@
+const postsResolvers = require('./post');
+const usersResolvers = require('./users');
+const commentsResolvers = require('./comments');
+
+module.exports = {
+    Post: {
+      likeCount: (parent) => parent.likes.length,
+      commentCount: (parent) => parent.comments.length
+    },
+    Query: {
+      ...postsResolvers.Query.getPost,
+      ...usersResolvers.Query
+    },
+    Mutation: {
+      ...usersResolvers.Mutation,
+      ...postsResolvers.Mutation,
+      ...commentsResolvers.Mutation
+    },
+  };
